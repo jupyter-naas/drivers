ec4f185d48e9c9c14203b629a1322747e7bd3b07
riderx <martindonadieu@gmail.com>
2020-09-26 01:15:57 +0200

    fix: bump version

--- naas_drivers/__init__.py
+++ naas_drivers/__init__.py
@@ -14,7 +14,7 @@ from .sentiment_analysis import SentimentAnalysis
 from .toucan import Toucan
 from .pandas_extra import PandasExtra
 
-__version__ = "0.0.15"
+__version__ = "0.0.16"
 
 darkknight = DarkKnight
 ftp = Ftp
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas-drivers",
-    version="0.0.15",
+    version="0.0.16",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     description="drivers made to easy connect to any services",
