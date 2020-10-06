9e7ec4b00d08fc72820e163a3f6d776000364cbd
GitHub Action <action@github.com>
2020-10-06 13:01:02 +0000

    bump: version 0.2.1b0 → 0.2.1b1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas-drivers",
-    version="0.2.1b0",
+    version="0.2.1b1",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
