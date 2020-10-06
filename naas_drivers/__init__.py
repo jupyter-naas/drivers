42d47f7507f4b4229ce952913d5343e4168988b7
GitHub Action <action@github.com>
2020-10-06 16:05:18 +0000

    bump: version 0.2.3 → 0.2.4

--- naas_drivers/__init__.py
+++ naas_drivers/__init__.py
@@ -13,7 +13,7 @@ from .prediction import Prediction
 from .sentiment_analysis import SentimentAnalysis
 from .toucan import Toucan
 
-__version__ = "0.2.3"
+__version__ = "0.2.4"
 
 darkknight = DarkKnight
 ftp = Ftp
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas-drivers",
-    version="0.2.3",
+    version="0.2.4",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
