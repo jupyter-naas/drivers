602aba8007c836e847a772ff5368f82ad33b2566
GitHub Action <action@github.com>
2020-10-13 09:43:03 +0000

    bump: version 0.9.1 → 0.9.2

--- naas_drivers/__init__.py
+++ naas_drivers/__init__.py
@@ -16,7 +16,7 @@ from .prediction import Prediction
 from .sentiment_analysis import SentimentAnalysis
 from .toucan import Toucan
 
-__version__ = "0.9.1"
+__version__ = "0.9.2"
 
 darkknight = DarkKnight
 ftp = Ftp
