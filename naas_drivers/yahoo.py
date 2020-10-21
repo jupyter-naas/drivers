49fad461a5618f2274a3a831be8d2caa423d43ee
riderx <martindonadieu@gmail.com>
2020-10-21 17:15:25 +0200

    feat: split plot in plotly and yahoo

---
+++ naas_drivers/yahoo.py
@@ -0,0 +1,47 @@
+import pandas as pd
+import datetime as dt
+
+
+class Yahoo:
+    def stock(
+        self,
+        stock_companies,
+        date_from=-36,
+        date_to="today",
+        interval="1d",
+        moving_averages=[],
+    ):
+        """ generate financial data """
+        if isinstance(stock_companies, str):
+            stock_companies = [stock_companies]
+        if isinstance(date_from, int) and date_from < 0:
+            date_from = dt.datetime.today() + dt.timedelta(days=date_from)
+        else:
+            raise ValueError(f"date_from ({date_from}) cannot be positive")
+        if isinstance(date_to, int) and date_to > 0:
+            date_to = dt.datetime.today() + dt.timedelta(days=date_to)
+        if isinstance(date_to, str) and date_to == "today":
+            date_to = dt.datetime.today()
+        else:
+            raise ValueError(f"date_to ({date_to}) cannot be negative")
+        stocks = None
+        period1 = date_from.strftime("%s")
+        period2 = date_to.strftime("%s")
+        for company in stock_companies:
+            url = (
+                f"https://query1.finance.yahoo.com/v7/finance/download/"
+                f"{company}?period1={period1}&period2={period2}&interval={interval}&events=history"
+            )
+            stock = pd.read_csv(url)
+            stock["Company"] = company
+            for moving_average in moving_averages:
+                stock[f"MA{moving_average}"] = stock.Close.rolling(
+                    moving_average
+                ).mean()
+            if stocks is None:
+                stocks = stock
+            else:
+                stocks = stocks.append(stock)
+            print("getted data for", company, url)
+
+        return stocks
