d9f4110c5459e981fbd234193583f39255e55f50
riderx <martindonadieu@gmail.com>
2020-11-03 12:10:46 +0100

    fix: bobapp functions location

--- naas_drivers/in_out_drivers/bobapp.py
+++ naas_drivers/in_out_drivers/bobapp.py
@@ -128,7 +128,7 @@ class SmartTable(CRUD):
         endpoint = f"smarttables/{database}/{collection}"
         CRUD.__init__(self, base_url, endpoint, auth)
 
-    def allowed(self, database, collection):
+    def allowed(self):
         req = requests.post(
             url=f"{self.base_public_url}/{self.endpoint}/allowed",
             auth=self.auth,
@@ -150,50 +150,24 @@ class SmartTable(CRUD):
         return req
 
 
-class Bobapp(In_Driver, Out_Driver):
-
-    base_public_url = None
-    smart_tables = []
-    __auth = None
-    req_headers = {"Accept": "application/json", "Content-Type": "application/json"}
-    users = None
-    workspaces = None
-    me = None
-
-    def connect(self, api_key=None, user=None, BOBAPP_API=None):
-        """
-        Description: This class connect you to a Bobapp instance
-        """
-        self.user = user if user else os.environ.get("JUPYTERHUB_USER", user)
-        self.base_public_url = (
-            BOBAPP_API if BOBAPP_API else os.environ.get("BOBAPP_API", BOBAPP_API)
-        )
-        if api_key:
-            self.__auth = HTTPBasicAuth(self.user, api_key)
-        self.users = CRUD(self.base_public_url, "users", self.__auth)
-        self.workspaces = CRUD(self.base_public_url, "workspaces", self.__auth)
-        self.me = Me(self.base_public_url, self.__auth)
-        self.connected = True
-        return self
-
-    def connect_smarttable(self, database, collection):
-        self.check_connect()
-        return SmartTable(self.base_public_url, database, collection, self.__auth)
+class Users(CRUD):
+    def __init__(self, base_url, auth):
+        CRUD.__init__(self, base_url, "users", auth)
 
     def validate_email(self, email):
         regex_email = r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)"
         return not re.match(regex_email, email)
 
-    def get_user(self, email):
+    def get_by_email(self, email):
         # Get user
         search = {"email": email}
-        req = self.users.get_all(search=search, limit=1)
+        req = self.get_all(search=search, limit=1)
         users = req.json()
         if len(users) == 0:
             return None
         return users[0]
 
-    def update_user(
+    def create_or_update(
         self,
         email,
         password,
@@ -204,51 +178,44 @@ class Bobapp(In_Driver, Out_Driver):
         user_role=None,
     ):
 
-        # Init variable
-        check_role = False
-        check_user = False
-
         # -> email
         # Delete space: email must have no space at the begining or end
         email = email.strip().lower()
 
         # -> role
         if role not in ["user", "admin"]:
-            check_role = False
-            print(f"Role {role} not recognized in Bobapp")
+            raise ValueError(f"Role {role} not recognized in Bobapp")
+
+        if not self.validate_email(email):
+            raise ValueError(f"User {email} not valid")
+
+        # Init user info
+        new_user = {
+            "email": email,
+            "password": password,
+            "firstName": first_name,
+            "lastName": last_name,
+            "role": role,
+            "phoneNumber": phone_number,
+            "userRole": user_role,
+        }
+
+        # Get user in Bobapp
+        users = self.get_by_email(email)
+
+        # If user does not exist => create user
+        if len(users) == 0:
+            user = new_user
+            self.insert(user)
+            print(f"User {email} created in Bobapp, password: {password}.")
         else:
-            check_role = True
+            # user = users[0]
+            new_user["_id"] = users[0]["_id"]
+            self.update(new_user)
+            print(f"User {email} updated in Bobapp, password: {password}.")
+        return True
 
-        # Create user in Bobapp
-        if check_role and self.validate_email(email):
-            check_user = True
-            # Init user info
-            new_user = {
-                "email": email,
-                "password": password,
-                "firstName": first_name,
-                "lastName": last_name,
-                "role": role,
-                "phoneNumber": phone_number,
-                "userRole": user_role,
-            }
-
-            # Get user in Bobapp
-            users = self.get_user(email)
-
-            # If user does not exist => create user
-            if len(users) == 0:
-                user = new_user
-                self.users.insert(user)
-                print(f"User {email} created in Bobapp, password: {password}.")
-            else:
-                # user = users[0]
-                new_user["_id"] = users[0]["_id"]
-                self.users.update(new_user)
-                print(f"User {email} updated in Bobapp, password: {password}.")
-        return check_user
-
-    def update_user_workspace(
+    def update_workspace(
         self, email, workspace_id=None, workspace_name=None, default=False
     ):
         # -> email
@@ -258,7 +225,7 @@ class Bobapp(In_Driver, Out_Driver):
         if not self.validate_email(email):
             raise ValueError(f"User {email} not valid")
 
-        user = self.get_user(email)
+        user = self.get_by_email(email)
         if not user:
             raise ValueError(f"User {email} does not exist in Bobapp")
 
@@ -282,7 +249,7 @@ class Bobapp(In_Driver, Out_Driver):
         if workspace_id is not None and not user["workspaces"].get(workspace_id, None):
             user["workspaces"] = {}
             user["workspaces"][workspace_id] = workspace_param
-            self.users.update(user)
+            self.update(user)
             print(
                 f"Workspace {workspace_id} : '{workspace_name}' added to user {email}"
             )
@@ -295,9 +262,9 @@ class Bobapp(In_Driver, Out_Driver):
                 "section": 2,
                 "id": workspace_id,
             }
-            self.users.update(user)
+            self.update(user)
 
-    def update_user_service(self, serv, email, password):
+    def update_service(self, serv, email, password):
 
         # Check service
         serv_list = ["ftp", "jupyter", "wekan"]
@@ -311,7 +278,7 @@ class Bobapp(In_Driver, Out_Driver):
         if not self.validate_email(email):
             raise ValueError(f"User {email} not valid")
 
-        user = self.get_user(email)
+        user = self.get_by_email(email)
         if not user:
             raise ValueError(f"User {email} does not exist in Bobapp")
         # Init service
@@ -328,12 +295,12 @@ class Bobapp(In_Driver, Out_Driver):
             user["services"] = {}
         if not user["services"].get(serv, None):
             user["services"][serv] = service
-            self.users.update(user)
+            self.update(user)
             print(f"Service {serv} Added to user", email)
         else:
             print(f"Service already exists in Bobapp for {email}")
 
-    def create_user_service(self, serv, email, password):
+    def create_service(self, serv, email, password):
 
         # Create login
         email = email.strip().lower()
@@ -374,7 +341,7 @@ class Bobapp(In_Driver, Out_Driver):
             headers = {"Authorization": AUTH_TOKEN_JUP}
             requests.post(URI_JUP, data=login, headers=headers).json()
 
-    def create_user(
+    def create(
         self,
         email,
         password,
@@ -386,13 +353,44 @@ class Bobapp(In_Driver, Out_Driver):
         services,
     ):
         # Create user in Bobapp
-        check_dk = self.update_user(email, password, first_name, last_name, role)
+        check_dk = self.create_or_update(email, password, first_name, last_name, role)
 
         # Add workspace default to user
         if check_dk:
-            self.update_user_workspace(email, workspace_id, workspace_name, True)
+            self.update_workspace(email, workspace_id, workspace_name, True)
 
             # Create service
             for serv in services:
-                self.create_user_service(serv, email, password)
-                self.update_user_service(serv, email, password)
+                self.create_service(serv, email, password)
+                self.update_service(serv, email, password)
+
+
+class Bobapp(In_Driver, Out_Driver):
+
+    base_public_url = None
+    smart_tables = []
+    __auth = None
+    req_headers = {"Accept": "application/json", "Content-Type": "application/json"}
+    users = None
+    workspaces = None
+    me = None
+
+    def connect(self, api_key=None, user=None, BOBAPP_API=None):
+        """
+        Description: This class connect you to a Bobapp instance
+        """
+        self.user = user if user else os.environ.get("JUPYTERHUB_USER", user)
+        self.base_public_url = (
+            BOBAPP_API if BOBAPP_API else os.environ.get("BOBAPP_API", BOBAPP_API)
+        )
+        if api_key:
+            self.__auth = HTTPBasicAuth(self.user, api_key)
+        self.users = Users(self.base_public_url, self.__auth)
+        self.workspaces = CRUD(self.base_public_url, "workspaces", self.__auth)
+        self.me = Me(self.base_public_url, self.__auth)
+        self.connected = True
+        return self
+
+    def connect_smarttable(self, database, collection):
+        self.check_connect()
+        return SmartTable(self.base_public_url, database, collection, self.__auth)
