f55f7d8a42abeee1a5a2f8fb64583e3088f2db89
GitHub Action <action@github.com>
2020-11-03 20:32:42 +0000

    bump: version 0.24.21 → 0.24.22

--- naas_drivers/__init__.py
+++ naas_drivers/__init__.py
@@ -24,7 +24,7 @@ from naas_drivers.inputOutput.toucan import Toucan
 import requests
 import os
 
-__version__ = "0.24.21"
+__version__ = "0.24.22"
 
 __github_repo = "jupyter-naas/drivers"
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas-drivers",
-    version="0.24.21",
+    version="0.24.22",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
