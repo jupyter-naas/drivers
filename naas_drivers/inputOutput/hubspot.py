e1f6060495d25f0f4ac6df1b633f88d044c0546e
FlorentLvr <florent.ravenel1@gmail.com>
2020-11-25 11:33:36 +0100

    fix: update hubspot driver

--- naas_drivers/inputOutput/hubspot.py
+++ naas_drivers/inputOutput/hubspot.py
@@ -1,8 +1,9 @@
 from naas_drivers.driver import InDriver, OutDriver
 import pandas as pd
 import requests
-import datetime
+from datetime import datetime
 import os
+import string
 
 class HSCRUD:
     # class HSCRUD(CRUD):
@@ -12,37 +13,40 @@ class HSCRUD:
         self.base_url = base_url
         self.model_name = self.base_url.split("/")[-1]
         
+        # Manage message
+        message_dict = {"contacts": "Contact",
+                        "company": "Company",
+                        "deals": "Deal"}
+        self.msg = message_dict[self.model_name]
+        
     def __values_format(self, data):
         for key, value in data['properties'].items():
-            print(value)
-            print(key)
-            if key == 'filename':
-                value = string.capwords(str(value).strip().replace('-',' ')).replace(' ','-')
-            elif key == 'lastname':
-                value = str(value).strip().upper()
-            elif key == 'phone':
-                if value == None:
-                    value = ""
-                else:
-                    value = str(value).strip().replace(' ','').replace('.','')
-            elif key == 'closedate':
-                check_closedate = True
-                # Check if close date is in correct format
-                if not value is None:
-                    if len(value) == 0:
-                        print(f"Close date '{value}' is empty. Please fill in the function.")
-                        check_closedate = False
-                    else:
-                        try:
-                            value = datetime.datetime.strptime(value, "%d/%m/%Y")
-                            value = str(int(closedate.timestamp()))+"000"
-                        except:
-                            check_closedate = False
-                            print(f"Close date '{value}' is not in the correct format.\n"
-                                   "Please change it to %d/%m/%Y.")
-            else:
-                if value != None:
-                    value = value.strip()
+            # Check if value is not None
+            if not value is None:
+                # Force format to string  & delete space
+                value = str(value).strip()
+            
+                # Specific rules
+                if key == 'firstname':
+                    value = string.capwords(value.replace('-',' ')).replace(' ','-')
+                elif key == 'lastname':
+                    value = value.upper()
+                elif key == 'phone':
+                    value = value.replace(' ','').replace('.','')
+                elif key == 'closedate':
+                    try:
+                        value = datetime.strptime(value, "%d/%m/%Y")
+                        value = str(int(value.timestamp()))+"000"
+                    except:
+                        print(f"Close date '{value}' is not in the correct format.\n"
+                               "Please change it to %d/%m/%Y.")
+                elif key == 'amount':
+                    value = value.replace('.','')
+                    
+                # Change value in dict
+                data['properties'][key] = value
+                
+        return data
 
     def __get_by_page(self, params):
         req = requests.get(
@@ -73,59 +77,46 @@ class HSCRUD:
         return df
 
     def get(self, uid):
-        if not uid:
-            print(f"Uid can not be None.")
-        else:
-            try:
-                req = requests.get(
-                    url=f"{self.base_url}/{uid}",
-                    headers=self.req_headers,
-                    params=self.params,
-                    allow_redirects=False,
-                )
-                req.raise_for_status()
-                # Message success
-                print(f"id '{uid}' successfully recovered.")
-                return req.json()
-            except requests.HTTPError as err:
-                err_code = err.response.status_code
-                if err_code == 404:
-                    print(f"id '{uid}' does not exist. Please, fill the function with an existing id.")
-                else:
-                    print(err_code)
+        try:
+            req = requests.get(
+                url=f"{self.base_url}/{uid}",
+                headers=self.req_headers,
+                params=self.params,
+                allow_redirects=False,
+            )
+            req.raise_for_status()
+            res = req.json()
+            return res
+        except requests.HTTPError as err:
+            err_code = err.response.status_code
+            if err_code == 404:
+                print(f"{self.msg} id='{uid}' does not exist.")
 
-    def update(self, uid, data):
-        if len(uid) == 0:
-            print(f"uid is empty")
-        else:
-            try:
-                data = self.__values_format(data)
-                req = requests.patch(
-                    url=f"{self.base_url}/{uid}",
-                    headers=self.req_headers,
-                    params=self.params,
-                    json=data,
-                    allow_redirects=False,
-                )
-                req.raise_for_status()
-                # Message success
-                print(f"id '{uid}' successfully updated ({email})")
-                return req.json()
-            except requests.HTTPError as err:
-                err_code = err.response.status_code
-                if err_code == 404:
-                    print(f"id '{uid}' does not exist. Please, fill the function with an existing id.")
-                if err_code == 409:
-                     print(f"'{email}' with id '{uid}' already exists.")
-                elif err_code == 400:
-                     print(f"'Bad Request : syntax error on '{email}'. Please, check the syntax.")
-                else:
-                    print(err_code)
-        
+    def patch(self, uid, data):
+        data = self.__values_format(data)
+        try:
+            req = requests.patch(
+                url=f"{self.base_url}/{uid}",
+                headers=self.req_headers,
+                params=self.params,
+                json=data,
+                allow_redirects=False,
+            )
+            req.raise_for_status()
+            res = req.json()
+            # Message success
+            print(f"{self.msg} (id={uid}) successfully updated.")
+            return res
+        except requests.HTTPError as err:
+            err_code = err.response.status_code
+            err_msg = err.response.json()
+            if err_code == 404:
+                print(f"{self.msg} id='{uid}' does not exist.")
+            else:
+                print(f"{err_code}: {err_msg}")
             
     def send(self, data):
-        if len(data) == 0:
-            print(f"Data is empty")
+        data = self.__values_format(data)
         try:
             req = requests.post(
                 url=f"{self.base_url}/",
@@ -135,112 +126,93 @@ class HSCRUD:
                 allow_redirects=False,
             )
             req.raise_for_status()
-            data = req.json()
-            data_id = data['id']
+            res = req.json()
+            uid = res['id']
             # Message success
-            print(f"id '{data_id}' successfully created in Hubspot.")
-            return data_id
+            print(f"{self.msg} (id={uid}) successfully created.")
+            return uid
         except requests.HTTPError as err:
             err_code = err.response.status_code
-            if err_code == 404:
-                print(f"id '{uid}' does not exist. Please, fill the function with an existing id.")
-            if err_code == 409:
-                print(f"'{email}' with id '{uid}' already exists.")
-            elif err_code == 400:
-                print(f"'Bad Request : syntax error on '{email}'. Please, check the syntax.")
-            else:
-                print(err_code)
-
+            err_msg = err.response.json()
+            print(f"{err_code}: {err_msg}")
 
     def delete(self, uid):
-        if uid == None:
-            print(f"uid is empty")
-        else:
-            req = requests.delete(
-                url=f"{self.base_url}{uid}",
-                headers=self.req_headers,
-                params=self.params,
-                allow_redirects=False,
-            )
-            req.raise_for_status()
-            # Message success
-            print(f"id {uid} deleted in Hubspot.")
+        res = self.get(uid)
+        if not res is None:
+            try:
+                req = requests.delete(
+                    url=f"{self.base_url}/{uid}",
+                    headers=self.req_headers,
+                    params=self.params,
+                    allow_redirects=False,
+                )
+                req.raise_for_status()
+                # Message success
+                print(f"{self.msg} (id={uid}) deleted.")
+            except requests.HTTPError as err:
+                err_code = err.response.status_code
+                err_msg = err.response.json()
+                print(f"{err_code}: {err_msg}")
 
 class Contact(HSCRUD):
-    def update_contact(self, uid, email=None, firstname=None, lastname=None, company=None, phone=None, jobtitle=None, hubspot_owner_id=None, website=None):     
+    def update(self, uid, email=None, firstname=None, lastname=None, phone=None, jobtitle=None, website=None, company=None, hubspot_owner_id=None):     
         data = {"properties": 
                   {
                     "email": email,
                     "firstname": firstname,
                     "lastname": lastname,
-                    "website": website,
-                    "company": company,
                     "phone": phone,
                     "jobtitle": jobtitle,
+                    "website": website,
+                    "company": company,
                     "hubspot_owner_id": hubspot_owner_id,
                    }
                  }
-        result = self.update(uid, data)
-        return result
+        res = self.patch(uid, data)
+        return res
         
-    def create(self, email, firstname, lastname, company=None, phone=None, jobtitle=None, hubspot_owner_id=None, website=None):
+    def create(self, email, firstname=None, lastname=None, phone=None, jobtitle=None, website=None, company=None, hubspot_owner_id=None):
         data = {"properties": 
                   {
                     "email": email,
                     "firstname": firstname,
                     "lastname": lastname,
+                    "phone": phone,
+                    "jobtitle": jobtitle,
                     "website": website,
                     "company": company,
-                    "phone": phone,
                     "hubspot_owner_id": hubspot_owner_id,
                    }
                  }
-        result = self.send(data)
-        return result
+        res = self.send(data)
+        return res
     
 class Deal(HSCRUD):
-    def update_deal(self, uid, dealname=None, dealstage=None, closedate=None, hubspot_owner_id=None, pipeline=None, amount=None):
-        data = {"properties": 
+    def update(self, uid, dealname=None, dealstage=None, closedate=None, amount=None, hubspot_owner_id=None):
+        data = {"properties":
                   {
                     "dealstage": dealstage,
                     "dealname": dealname,
-                    "pipeline": pipeline,
-                    "amount": amount,
                     "closedate": closedate,
+                    "amount": amount,
                     "hubspot_owner_id": hubspot_owner_id,
                    }
                  }
-        result = self.update(uid, data)
-        return result
+        res = self.patch(uid, data)
+        return res
     
-    def create(self, dealname, dealstage, closedate=None, hubspot_owner_id=None, pipeline=None, amount=None):
-        check_closedate = True
-        # Check if close date is in correct format
-        if not closedate is None:
-            if len(closedate) == 0:
-                print(f"Close date '{closedate}' is empty. Please fill in the function.")
-                check_closedate = False
-            else:
-                try:
-                    closedate = datetime.datetime.strptime(closedate, "%d/%m/%Y")
-                    closedate = str(int(closedate.timestamp()))+"000"
-                except:
-                    check_closedate = False
-                    print(f"Close date '{closedate}' is not in the correct format.\n"
-                           "Please change it to %d/%m/%Y.")
-        if check_closedate:
-            data = {"properties": 
-                      {
-                        "dealstage": dealstage,
-                        "dealname": dealname,
-                        "pipeline": pipeline,
-                        "amount": amount,
-                        "closedate": closedate,
-                        "hubspot_owner_id": hubspot_owner_id,
-                       }
-                     }
-            result = self.send(data)
-            return result
+    def create(self, dealname, dealstage, closedate=None, amount=None, hubspot_owner_id=None):
+        data = {"properties":
+                  {
+                    "dealstage": dealstage,
+                    "dealname": dealname,
+                    "closedate": closedate,
+                    "amount": amount,
+                    "hubspot_owner_id": hubspot_owner_id,
+                   }
+                 }
+        res = self.send(data)
+        return res
     
 class Pipeline:
     def __init__(self, base_url, auth, req_headers, params):
@@ -306,41 +278,65 @@ class Association:
         self.base_url = base_url
         self.model_name = self.base_url.split("/")[-1]
 
-    def get(self, objet, objet_id, associate):
-        try:
-            req = requests.get(
-                url=f"{self.base_url}{objet}/{objet_id}/associations/{associate}",
-                headers=self.req_headers,
-                params=self.params,
-                allow_redirects=False,
-            )
-            req.raise_for_status()
-            data = req.json()
-            df = pd.DataFrame.from_records(data['results'])
-            return df
-        except requests.HTTPError as err:
-            err_code = err.response.status_code
-            if err_code == 400:
-                raise ValueError(f"Bad Request : syntax error on '{objet}' or '{associate}'. Please check the possibilities : 'contact', 'deal', 'company'")
-            else:
-                print(err.response.json())
+    def get(self, object_name, object_id, associate):
+        objects = ["deal"]
+        object_check = True
+        if not object_name in objects:
+            object_check = False
+            print(f"Object '{object_name}' does not exist. Please chose one in following list: {objects}")
+            
+        associates = ["contact"]
+        associate_check = True
+        if not associate in associates:
+            associate_check = False
+            print(f"Associate '{associate}' does not exist. Please chose one in following list: {associates}")
+            
+        if object_check and associate_check:
+            try:
+                req = requests.get(
+                    url=f"{self.base_url}/{object_name}/{object_id}/associations/{associate}",
+                    headers=self.req_headers,
+                    params=self.params,
+                    allow_redirects=False,
+                )
+                req.raise_for_status()
+                data = req.json()
+                df = pd.DataFrame.from_records(data['results'])
+                if len(df) == 0:
+                    print(f"Object id='{object_id}' does not have any associates")
+                return df
+            except requests.HTTPError as err:
+                err_code = err.response.status_code
+                err_msg = err.response.json()
+                print(f"{err_code}: {err_msg}")
     
-    def send(self, objet, objet_id, associate, id_associate, associationType):
-        try:
-            req = requests.put(
-                url=f"{self.base_url}{objet}/{objet_id}/associations/{associate}/{id_associate}/{associationType}",
-                headers=self.req_headers,
-                params=self.params,
-                allow_redirects=False,
-            )
-            req.raise_for_status()
-            print(f"{objet} '{objet_id}' and {associate} '{id_associate}' successfully associated !")
-        except requests.HTTPError as err:
-            err_code = err.response.status_code
-            if err_code == 400:
-                print(f"Bad Request : syntax error on '{objet}' or '{associate}'. Please check the possibilities : 'contact', 'deal', 'company'")
-            else:
-                print(err.response.json())
+    def create(self, object_name, object_id, associate, id_associate):
+        objects = ["deal"]
+        object_check = True
+        if not object_name in objects:
+            object_check = False
+            print(f"Object '{object_name}' does not exist. Please chose one in following list: {objects}")
+            
+        associates = ["contact"]
+        associate_check = True
+        if not associate in associates:
+            associate_check = False
+            print(f"Associate '{associate}' does not exist. Please chose one in following list: {associates}")
+            
+        if object_check and associate_check:
+            try:
+                req = requests.put(
+                    url=f"{self.base_url}/{object_name}/{objet_id}/associations/{associate}/{id_associate}/{object_name}_to_{associate}",
+                    headers=self.req_headers,
+                    params=self.params,
+                    allow_redirects=False,
+                )
+                req.raise_for_status()
+                print(f"{object_name} '{object_id}' and {associate} '{id_associate}' successfully associated !")
+            except requests.HTTPError as err:
+                err_code = err.response.status_code
+                err_msg = err.response.json()
+                print(f"{err_code}: {err_msg}")
 
 class Hubspot(InDriver, OutDriver):
 
@@ -359,12 +355,12 @@ class Hubspot(InDriver, OutDriver):
                        "hapikey": api_token}
         
         # Init end point
-        self.contacts = Contact(f"{self.base_url}/objects/contacts/", self.token, self.req_headers, self.params)
-        self.company = HSCRUD(f"{self.base_url}/objects/company/", self.token, self.req_headers, self.params)
-        self.deals = Deal(f"{self.base_url}/objects/deals/", self.token, self.req_headers, self.params)
+        self.contacts = Contact(f"{self.base_url}/objects/contacts", self.token, self.req_headers, self.params)
+        self.company = HSCRUD(f"{self.base_url}/objects/company", self.token, self.req_headers, self.params)
+        self.deals = Deal(f"{self.base_url}/objects/deals", self.token, self.req_headers, self.params)
         self.pipelines = Pipeline_deal(f"{self.base_url}/pipelines/deals", self.token, self.req_headers, self.params)
         self.dealstages = Dealstage(f"{self.base_url}/pipelines/deals", self.token, self.req_headers, self.params)
-        self.associations = Association(f"{self.base_url}/objects/", self.token, self.req_headers, self.params)
+        self.associations = Association(f"{self.base_url}/objects", self.token, self.req_headers, self.params)
         
         # Set connexion to active
         self.connected = True
