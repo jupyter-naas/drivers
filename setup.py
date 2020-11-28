5928211a71b731ae6472cb089baac400ca72a7be
riderx <martindonadieu@gmail.com>
2020-11-28 12:04:07 +0000

    feat: add get attachement

--- setup.py
+++ setup.py
@@ -27,6 +27,7 @@ setup(
         ]
     },
     install_requires=[
+        "imap_tools>=0,<1",
         "markdown2>=2,<3",
         "newsapi-python>=0,<1",
         "airtable-python-wrapper>=0,<1",
