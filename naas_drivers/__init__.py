28fdc50a4c78a448bb903b7f6b13fae8f3dba748
GitHub Action <action@github.com>
2021-01-05 14:16:41 +0000

    bump: version 0.38.0b3 → 0.38.0b4

--- naas_drivers/__init__.py
+++ naas_drivers/__init__.py
@@ -30,7 +30,7 @@ from naas_drivers.inputOutput.thinkific import Thinkific
 import requests
 import os
 
-__version__ = "0.38.0b3"
+__version__ = "0.38.0b4"
 
 __github_repo = "jupyter-naas/drivers"
 
