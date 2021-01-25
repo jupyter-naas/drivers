7c7c6b387bc0f33f991988c46ae0796005acab8f
riderx <martindonadieu@gmail.com>
2021-01-25 20:15:17 +0000

    feat: allow send image in teams

--- naas_drivers/output/teams.py
+++ naas_drivers/output/teams.py
@@ -11,8 +11,14 @@ class Teams(OutDriver):
         self.connected = True
         return self
 
-    def send(self, text):
+    def send(self, text, title=None, image=None, imageTitle=None):
         self.check_connect()
         self.client.text(text)
+        if title:
+            self.client.title(title)
+        if image:
+            myMessageSection = pymsteams.cardsection()
+            myMessageSection.addImage(image, ititle=imageTitle)
+            self.client.addSection(myMessageSection)
         self.client.send()
         print("Message Send")
