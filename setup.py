969726b55e4dc7e8e7123df8c26e2885ea8be675
riderx <martindonadieu@gmail.com>
2021-01-25 19:45:15 +0000

    fix: add missing module

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
     install_requires=[
         "imap_tools>=0,<1",
         "slackclient>=2,<3",
+        "pymsteams>=0,<1",
         "pdfkit>=0,<1",
         "markdown2>=2,<3",
         "newsapi-python>=0,<1",
