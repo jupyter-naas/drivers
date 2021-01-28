9b84202b3d284049cfd016d8d015d60e0cb324e2
riderx <martindonadieu@gmail.com>
2021-01-28 17:19:42 +0000

    fix: refactore Zappier and integromat make it work like all

--- naas_drivers/output/integromat.py
+++ naas_drivers/output/integromat.py
@@ -3,9 +3,15 @@ import requests
 
 
 class Integromat(OutDriver):
-    def send(self, url, data=None):
+    def connect(self, url):
+        self._key = url
+        self.connected = True
+        return self
+
+    def send(self, data=None):
+        self.check_connect()
         r = requests.post(
-            url=url,
+            url=self._key,
             json=data,
         )
         r.raise_for_status()
--- naas_drivers/output/zappier.py
+++ naas_drivers/output/zappier.py
@@ -3,9 +3,15 @@ import requests
 
 
 class Zappier(OutDriver):
-    def webhook(self, url, data=None):
+    def connect(self, url):
+        self._key = url
+        self.connected = True
+        return self
+
+    def send(self, data=None):
+        self.check_connect()
         r = requests.post(
-            url=url,
+            url=self._key,
             json=data,
         )
         r.raise_for_status()
