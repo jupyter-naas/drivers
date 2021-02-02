0d018cab21081e32c2d2bcc890dc27eadbc5662f
GitHub Action <action@github.com>
2021-02-02 12:12:03 +0000

    bump: version 0.41.5 → 0.42.0

--- naas_drivers/__init__.py
+++ naas_drivers/__init__.py
@@ -34,7 +34,7 @@ from naas_drivers.input.markdown import Markdown
 import requests
 import os
 
-__version__ = "0.41.5"
+__version__ = "0.42.0"
 
 __github_repo = "jupyter-naas/drivers"
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas-drivers",
-    version="0.41.5",
+    version="0.42.0",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
