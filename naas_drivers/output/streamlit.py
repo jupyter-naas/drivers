efb245c2709fc7fc3597f3bdf98e4f62aa2147e6
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-20 09:15:37 -0700

    Update streamlit.py

--- naas_drivers/output/streamlit.py
+++ naas_drivers/output/streamlit.py
@@ -13,8 +13,8 @@ class BaseApp:
         for tunnel in active_tunnels:
             public_url = tunnel.public_url
             ngrok.disconnect(public_url)
-        url = ngrok.connect(port=self.port, options={"bind_tls": True}, return_ngrok_tunnel=True)
-        print(f'Web App can be accessed on: {url.public_url}')
+        url = ngrok.connect(port=self.port, options={"bind_tls": True})
+        print(f'Web App can be accessed on: {url}')
 
 
 class NaasStreamlit(BaseApp):
