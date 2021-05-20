3cac5273a45310ddbdaeedd2d5ca6172152a6b9c
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-19 23:40:33 -0700

    Update streamlit.py

--- naas_drivers/output/streamlit.py
+++ naas_drivers/output/streamlit.py
@@ -18,6 +18,11 @@ class BaseApp():
 
 
 class Streamlit(BaseApp):
+    def __init__(self, path, port=9999):
+        super().__init__(port)
+        self.path = path
+        self.run_app()
+
     def run_app(self, debug=True):
         os.system(f"fuser -n tcp -k {self.port}")
         cmd = f'streamlit run {self.path} --server.port {self.port}'
@@ -31,8 +36,3 @@ class Streamlit(BaseApp):
             for line in proc.stdout:
                 if debug:
                     print(line, end="")
-
-    def __init__(self, path, port=9999):
-        super().__init__(port)
-        self.path = path
-        self.run_app()
