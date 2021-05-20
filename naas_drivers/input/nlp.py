ab07bc4d1d7807c73e275bd63774bf95f7f35125
Gagan Bhatia <49101362+gagan3012@users.noreply.github.com>
2021-05-19 20:05:50 -0700

    Update nlp.py

--- naas_drivers/input/nlp.py
+++ naas_drivers/input/nlp.py
@@ -1,13 +1,31 @@
+from typing import Optional, Union
+
+from transformers import PretrainedConfig, PreTrainedTokenizer
+
 from naas_drivers.driver import InDriver
-import torch
-from transformers.pipelines.question_answering import QuestionAnsweringArgumentHandler, QuestionAnsweringPipeline
+from transformers.pipelines.question_answering import QuestionAnsweringPipeline
 from transformers.pipelines.text2text_generation import SummarizationPipeline, Text2TextGenerationPipeline
 from transformers.pipelines.text_classification import TextClassificationPipeline
 from transformers.pipelines.text_generation import TextGenerationPipeline
 from transformers.file_utils import is_torch_available, is_tf_available
 
+if is_torch_available():
+    import torch
+    from transformers.models.auto.modeling_auto import (
+        AutoModelForSequenceClassification,
+        AutoModelForSeq2SeqLM,
+        AutoModelForCausalLM,
+        AutoModelForQuestionAnswering,
+    )
 
-
+if is_tf_available():
+    import tensorflow as tf
+    from transformers.models.auto.modeling_tf_auto import (
+        TFAutoModelForSequenceClassification,
+        TFAutoModelForSeq2SeqLM,
+        TFAutoModelForCausalLM,
+        TFAutoModelForQuestionAnswering,
+    )
 
 TASKS = {
     "text-classification": {
@@ -47,4 +65,21 @@ TASKS = {
         "pt": AutoModelForSeq2SeqLM if is_torch_available() else None,
         "default": {"model": {"pt": "sshleifer/distilbart-cnn-12-6", "tf": "t5-small"}},
     },
+
 }
+
+
+class NLP(InDriver):
+    def get(
+            self,
+            task: str,
+            model: Optional = None,
+            config: Optional[Union[str, PretrainedConfig]] = None,
+            tokenizer: Optional[Union[str, PreTrainedTokenizer]] = None,
+    ):
+        if task not in TASKS:
+            raise KeyError(
+                "Unknown task {}, available tasks are {}".format(
+                    task, list(TASKS.keys())
+                )
+            )
