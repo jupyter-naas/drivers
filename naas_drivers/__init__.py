fa04972aed7d34978b0585efc42487106ef3fd8e
GitHub Action <action@github.com>
2021-08-31 15:11:52 +0000

    bump: version 0.70.6 → 0.70.7

--- naas_drivers/__init__.py
+++ naas_drivers/__init__.py
@@ -37,7 +37,7 @@ from naas_drivers.input.huggingface import Huggingface
 import requests
 import os
 
-__version__ = "0.70.6"
+__version__ = "0.70.7"
 
 __github_repo = "jupyter-naas/drivers"
 
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="naas-drivers",
-    version="0.70.6",
+    version="0.70.7",
     author="Martin Donadieu",
     author_email="martindonadieu@gmail.com",
     license="BSD",
