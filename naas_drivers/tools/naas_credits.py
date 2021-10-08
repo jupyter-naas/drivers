51ca8e694553ff1f42d4d05b3f609216ce70d335
Maxime Jublou <maxime@jublou.fr>
2021-10-08 09:25:44 +0200

    feat: add naascredits.transaction.get

--- naas_drivers/tools/naas_credits.py
+++ naas_drivers/tools/naas_credits.py
@@ -1,5 +1,6 @@
 from naas_drivers.driver import InDriver, OutDriver
 from naas_drivers.tools.naas_auth import NaasAuth
+from datetime import datetime
 import requests as r
 
 CREDITS_API_FQDN = 'credits.naas.ai'
@@ -40,6 +41,20 @@ class NaasCredits(InDriver, OutDriver):
         
         def get_currents(self):
             res = r.get(f'{AUTH_API_PROTOCOL}://{CREDITS_API_FQDN}/transactions/current', headers=self.headers)
+            res.raise_for_status()
+            return res.json()
+        
+        def get(self, page_size: int = None, page_number: int = None, start_date : datetime = None, end_date : datetime = None, order_by : str = None):
+            params = {
+                'page_size': page_size,
+                'page_number': page_number,
+                'start_date': start_date,
+                'end_date': end_date,
+                'order_by': order_by
+            }
+            params = {k: params[k] for k in params.keys() if params[k] != None}
+                
+            res = r.get(f'{AUTH_API_PROTOCOL}://{CREDITS_API_FQDN}/transactions', headers=self.headers, params=params)
             res.raise_for_status()
             return res.json()
         
