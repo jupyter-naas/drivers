3d5c354767459a4d50cb66bb6a0826d320564041
Justin Prather <justinprather94@gmail.com>
2021-11-17 08:49:56 -0700

    Cleaned up with pydoc comments

--- naas_drivers/tools/taggun.py
+++ naas_drivers/tools/taggun.py
@@ -1,56 +1,88 @@
 from naas_drivers.driver import OutDriver
-import requests
-import pandas as pd
-import os
 import hashlib
+import os
+import requests
 
 
 class Taggun(OutDriver):
+    '''
+    Naas Driver for Taggun.io
+
+    Taggun offers affordable Receipt/invoice OCR,
+        Input: a pdf or image of a receipt
+        Output: a dict containing parsed data from the image, and a corresponding
+            confidence level for the extraction
+    '''
     _key = None
     _fileName = None
     _filePath = None
     _url = None
     _incognito = None
     _hash = None
-    
+
     data = None
 
-    def connect(self, key, file, mode = 'simple', incognito = False):
+    def connect(self, key, file, mode="simple", incognito=False):
+        '''
+        Initialize all the necessary elements to make an api request
+
+        Args:
+            key (string): Taggun API key
+            file (string): Path to the file being sent for parsing
+            mode (string): Either simple or verbose. default simple
+            incognite (bool): If true, taggun will not store a copy of the
+                uploaded file for reinforcement learning. default false
+        '''
         self._key = key
         self.connected = True
-        self._url = f'https://api.taggun.io/api/receipt/v1/{mode}/file'
-        self._fileName = os.path.basename(file);
+        self._url = f"https://api.taggun.io/api/receipt/v1/{mode}/file"
+        self._fileName = os.path.basename(file)
         self._filePath = file
-        self._incognito = incognito;
+        self._incognito = incognito
         return self
-    
-    def read(self):
-        if not self._fileData:
-            self._fileData = open(self._filePath, 'rb')
-        return
-            
+
     def hashFile(self):
-        hasher = hashlib.sha256()
-        hasher.update(copy.copy(self._fileData).read())
-        self._hash = hasher.hexdigest()
+        '''
+        Create a hash of the file to be sent. Taggun allows user-submitted data
+        for reinforcement learning. This requires a unique identifier for the file.
+        Instead of generating a UUID, which would need to be stored, use a hash
+        of the file itself as a unique identifier
+
+        Returns:
+            hash of the file
+        '''
+        with open(self._filePath, "rb") as f:
+            self._hash = hashlib.sha256(f.read()).hexdigest()
         return self._hash
 
     def send(self):
+        '''
+        Dispatch the api request to Taggun and receive parsed data
+
+        Returns:
+            parsed data as a dict from Taggun
+        '''
         self.check_connect()
-        self.read()
         self.hashFile()
-        
-        headers = {'apikey': self._key}
-        files = {
-          'file': (
-            self._fileName,
-            self._fileData,
-            'application/pdf'), # content-type for the file
-          # other optional parameters for Taggun API (eg: incognito, refresh, ipAddress, language)
-          'incognito': (
-            None, #set filename to none for optional parameters
-            'false') #value for the parameters
-        }
-        response = requests.post(self._url, files=files, headers=headers)
-        self.data = response.json()
+
+        with open(self._filePath, "rb") as f:
+            headers = {"apikey": self._key}
+            files = {
+                "file": (
+                    self._fileName,
+                    f,
+                    "application/pdf",
+                ),  # content-type for the file
+                # other optional parameters for Taggun API (eg: incognito, refresh, ipAddress, language)
+                "incognito": (
+                    None,  # set filename to none for optional parameters
+                    self._incognito,
+                ),  # value for the parameters
+                "referenceID": (
+                    None,  # set filename to none for optional parameters
+                    self._hash,  # value for the parameters
+                ),
+            }
+            response = requests.post(self._url, files=files, headers=headers)
+            self.data = response.json()
         return self.data
