767b934a404b15aa5afe640b971b323d20c3461a
FlorentLvr <florent.ravenel1@gmail.com>
2021-12-01 11:08:59 +0100

    refactor : update yahoo driver with date_to negative and remove print

--- naas_drivers/tools/yahoofinance.py
+++ naas_drivers/tools/yahoofinance.py
@@ -1,52 +1,68 @@
-from naas_drivers.driver import InDriver
 import pandas as pd
 import datetime as dt
 
 
-class Yahoofinance(InDriver):
+class Yahoofinance:
     def get(
         self,
-        stock_companies,
+        tickers,
         date_from=-36,
         date_to="today",
         interval="1d",
-        moving_averages=[],
+        moving_averages: list = [],
+        moving_average_col="Close",
     ):
-        """ generate financial data """
-        if isinstance(stock_companies, str):
-            stock_companies = [stock_companies]
-        if isinstance(date_from, int) and date_from < 0:
-            date_from = dt.datetime.today() + dt.timedelta(days=date_from)
-        else:
-            error_text = f"date_from ({date_from}) cannot be positive"
-            self.print_error(error_text)
-            return None
-        if isinstance(date_to, int) and date_to > 0:
-            date_to = dt.datetime.today() + dt.timedelta(days=date_to)
-        if isinstance(date_to, str) and date_to == "today":
-            date_to = dt.datetime.today()
-        else:
-            error_text = f"date_to ({date_to}) cannot be negative"
-            self.print_error(error_text)
-            return None
-        stocks = None
+        """Generate financial data"""
+        # Init dataframe
+        df_stocks = pd.DataFrame()
+
+        # If tickers is string => change to list
+        if isinstance(tickers, str):
+            tickers = [tickers]
+        # Set date from
+        if isinstance(date_from, int):
+            if date_from < 0:
+                date_from = dt.datetime.today() + dt.timedelta(days=date_from)
+            else:
+                error_text = f"❌ date_from ({date_from}) cannot be positive."
+                print(error_text)
+        # Set date to
+        if isinstance(date_to, int):
+            if date_to < 0:
+                date_to = dt.datetime.today() + dt.timedelta(days=date_to)
+            else:
+                error_text = f"❌ date_from ({date_to}) cannot be positive."
+                print(error_text)
+        if isinstance(date_to, str):
+            if date_to == "today":
+                date_to = dt.datetime.today()
+            else:
+                error_text = f"❌ date_to cannot be ({date_to}). Try with 'today' or use an integer."
+                print(error_text)
         period1 = date_from.strftime("%s")
         period2 = date_to.strftime("%s")
-        for company in stock_companies:
+        for ticker in tickers:
             url = (
                 f"https://query1.finance.yahoo.com/v7/finance/download/"
-                f"{company}?period1={period1}&period2={period2}&interval={interval}&events=history"
+                f"{ticker}?period1={period1}&period2={period2}&interval={interval}&events=history"
             )
-            stock = pd.read_csv(url)
-            stock["Company"] = company
-            for moving_average in moving_averages:
-                stock[f"MA{moving_average}"] = stock.Close.rolling(
-                    moving_average
-                ).mean()
-            if stocks is None:
-                stocks = stock
-            else:
-                stocks = stocks.append(stock)
-            print("getted data for", company, url)
-        stocks["Date"] = pd.to_datetime(stocks["Date"], format="%Y-%m-%d")
-        return stocks
+            df = pd.read_csv(url)
+            df["Ticker"] = ticker
+            if len(moving_averages) > 0:
+                if moving_average_col in df.columns:
+                    for moving_average in moving_averages:
+                        if isinstance(moving_average, str):
+                            moving_average = int(moving_average)
+                        if isinstance(moving_average, int):
+                            df[f"MA{moving_average}"] = (
+                                df[moving_average_col].rolling(moving_average).mean()
+                            )
+                        else:
+                            error_text = f"❌ Moving average '{moving_average_col}' not recognize."
+                            print(error_text)
+                else:
+                    error_text = f"❌ We can not calculate moving averages. Columns '{moving_average_col}' does not exist in dataframe."
+                    print(error_text)
+            df_stocks = pd.concat([df_stocks, df])
+        df_stocks["Date"] = pd.to_datetime(df_stocks["Date"], format="%Y-%m-%d")
+        return df_stocks
