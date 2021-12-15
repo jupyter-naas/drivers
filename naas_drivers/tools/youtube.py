1a5435b148f48f4b8488d802b0183daeeb49d713
FlorentLvr <florent.ravenel1@gmail.com>
2021-12-15 20:03:12 +0100

    fix: lint

--- naas_drivers/tools/youtube.py
+++ naas_drivers/tools/youtube.py
@@ -214,7 +214,7 @@ class Transcript:
         text_len = len(text)
 
         while count * chunk_size < text_len:
-            chunks.append(text[count * chunk_size: count * chunk_size + chunk_size])
+            chunks.append(text[count * chunk_size:count * chunk_size + chunk_size])
             count += 1
         return chunks
 
