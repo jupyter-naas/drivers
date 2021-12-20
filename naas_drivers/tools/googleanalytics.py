c19f64750196c9679fbc94bb416e6873678282cd
Charles Demontigny <charles.demontigny@fluxion.ca>
2021-12-20 17:13:32 -0500

    add time on landing & ranking

--- naas_drivers/tools/googleanalytics.py
+++ naas_drivers/tools/googleanalytics.py
@@ -17,20 +17,17 @@ class GoogleAnalytics(InDriver, OutDriver):
 
     def __init__(self, view_id: str) -> None:
         self.view_id = view_id
-
-    def connect(self) -> None:
         credentials = service_account.Credentials.from_service_account_file(
-            os.getenv("GCP_SERVICE_ACCOUNT_JSON"), 
+            os.getenv("GCP_SERVICE_ACCOUNT_JSON"),
             scopes = ['https://www.googleapis.com/auth/analytics.readonly'])
         self.service = build('analyticsreporting', 'v4', credentials=credentials)
-        return self
 
     @staticmethod
     def _get_body(view_id: str,
                   date_ranges: List[dict],
                   metrics: List[dict],
                   pivots_dimensions: List[dict],
-                  dimensions: List[dict]=[{'name': 'ga:yearMonth'}]) -> dict:
+                  dimensions: str='ga:yearMonth') -> dict:
         """
         Create the body of the request to Google Analytics Reporting API V4.
 
@@ -43,10 +40,10 @@ class GoogleAnalytics(InDriver, OutDriver):
 
         Returns response in JSON format.
         """
-        return {'reportRequests': [{'viewId': view_id, 
+        return {'reportRequests': [{'viewId': view_id,
                             'dateRanges': date_ranges,
                             'metrics': metrics,
-                            'dimensions': dimensions,
+                            'dimensions': {"name": dimensions},
                             "pivots": [{"dimensions": pivots_dimensions,
                                         "metrics": metrics
                                        }]
@@ -60,9 +57,8 @@ class GoogleAnalytics(InDriver, OutDriver):
         date_ranges = {"startDate": start_date, "endDate": end_date}
         metrics = [{"expression": "ga:users"}]
         pivots_dimensions = [{"name": "ga:channelGrouping"}]
-        dimensions = [{"name": "ga:yearMonth"}]
         # Create body
-        body = self._get_body(self.view_id, date_ranges, metrics, pivots_dimensions, dimensions)
+        body = self._get_body(self.view_id, date_ranges, metrics, pivots_dimensions)
         # Fetch Data
         response = self.service.reports().batchGet(body=body).execute()
         # Format Output
@@ -80,9 +76,8 @@ class GoogleAnalytics(InDriver, OutDriver):
         date_ranges = {"startDate": start_date, "endDate": end_date}
         metrics = {"expression": "ga:bounceRate"}
         pivots_dimensions = {"name": "ga:channelGrouping"}
-        dimensions = {"name": "ga:yearMonth"}
         # Create body
-        body = self._get_body(self.view_id, date_ranges, metrics, pivots_dimensions, dimensions)
+        body = self._get_body(self.view_id, date_ranges, metrics, pivots_dimensions)
         # Fetch Data
         response = self.service.reports().batchGet(body=body).execute()
         # Format Output
@@ -93,6 +88,52 @@ class GoogleAnalytics(InDriver, OutDriver):
             columns={"ga:yearMonth": "year_month", "ga:bounceRate": "bounce_rate"}, inplace=True)
         return bounce_rate
 
+    def get_time_landing(self,
+                         start_date: str,
+                         end_date: str,
+                         landing_path: str="/") -> pd.DataFrame:
+        """
+        Get the average time on landing page.
+        """
+        # Setup Request Parameters
+        date_ranges = {"startDate": start_date, "endDate": end_date}
+        metrics = {"expression": "ga:avgTimeOnPage"}
+        pivots_dimensions = {"name": "ga:landingPagePath"}
+        # Create body
+        body = self._get_body(self.view_id, date_ranges, metrics, pivots_dimensions)
+        # Fetch Data
+        response = self.service.reports().batchGet(body=body).execute()
+        # Format Output
+        avg_time_landing = self.format_pivot(response)
+        if landing_path in avg_time_landing.columns:
+            avg_time_landing = avg_time_landing.loc[:, landing_path]
+        else:
+            raise KeyError(f"Landing Path ({landing_path}) is not an available url pattern.")
+        avg_time_landing.index.rename("year_month", inplace=True)
+        avg_time_landing.rename(columns={"ga:avgTimeOnPage": "avg_time_landing"}, inplace=True)
+        return avg_time_landing
+
+    def get_pageview(self, start_date: str, end_date: str) -> pd.DataFrame:
+        """
+        
+        """
+        # Setup Request Parameters
+        date_ranges = {"startDate": start_date, "endDate": end_date}
+        metrics = {"expression": "ga:pageviews"}
+        pivots_dimensions = {"name": "ga:pagePath"}
+        dimensions = 'ga:year'
+        # Create body
+        body = self._get_body(self.view_id, date_ranges, metrics, pivots_dimensions, dimensions)
+        # Fetch Data
+        response = self.service.reports().batchGet(body=body).execute()
+        # Format output
+        pageview = self.format_pivot(response)
+        pageview.columns = [page[0] for page in pageview.columns]
+        pageview = pageview.head(1).T
+        pageview.reset_index(inplace=True)
+        pageview.columns = ['pages', 'pageview']
+        return pageview
+
     @staticmethod
     def format_summary(response):
         """
@@ -100,7 +141,7 @@ class GoogleAnalytics(InDriver, OutDriver):
         """
         row_index_names = response['reports'][0]['columnHeader']['dimensions']
         row_index = [element['dimensions'] for element in response['reports'][0]['data']['rows']]
-        row_index_named = pd.MultiIndex.from_arrays(np.transpose(np.array(row_index)), 
+        row_index_named = pd.MultiIndex.from_arrays(np.transpose(np.array(row_index)),
                                                     names = np.array(row_index_names))
         # extract column names
         summary_column_names = [item['name'] for item in response['reports'][0]
@@ -108,11 +149,9 @@ class GoogleAnalytics(InDriver, OutDriver):
         # extract table values
         summary_values = [element['metrics'][0]['values']
                           for element in response['reports'][0]['data']['rows']]
-        # combine. I used type 'float' because default is object, and as far as I know, all values are numeric
-        df = pd.DataFrame(data = np.array(summary_values), 
-                        index = row_index_named, 
+        return pd.DataFrame(data = np.array(summary_values),
+                        index = row_index_named,
                         columns = summary_column_names).astype('float')
-        return df
 
     @staticmethod
     def format_pivot(response):
@@ -128,19 +167,18 @@ class GoogleAnalytics(InDriver, OutDriver):
         column_metrics = [item['metric']['name'] for item in response['reports'][0]
                         ['columnHeader']['metricHeader']['pivotHeaders'][0]['pivotHeaderEntries']]
         array = np.concatenate((np.array(top_header),
-                                np.array(column_metrics).reshape((len(column_metrics),1))), 
+                                np.array(column_metrics).reshape((len(column_metrics),1))),
                             axis = 1)
         column_index = pd.MultiIndex.from_arrays(np.transpose(array))
         # create row index
         row_index_names = response['reports'][0]['columnHeader']['dimensions']
         row_index = [ element['dimensions'] for element in response['reports'][0]['data']['rows']]
-        row_index_named = pd.MultiIndex.from_arrays(np.transpose(np.array(row_index)), 
+        row_index_named = pd.MultiIndex.from_arrays(np.transpose(np.array(row_index)),
                                                     names = np.array(row_index_names))
         # combine into a dataframe
-        df = pd.DataFrame(data = np.array(pivot_values), 
-                        index = row_index_named, 
+        return pd.DataFrame(data = np.array(pivot_values),
+                        index = row_index_named,
                         columns = column_index).astype('float')
-        return df
 
     def format_report(self, response):
         """
@@ -150,4 +188,4 @@ class GoogleAnalytics(InDriver, OutDriver):
         pivot = self.format_pivot(response)
         if pivot.columns.nlevels == 2:
             summary.columns = [['']*len(summary.columns), summary.columns]
-        return(pd.concat([summary, pivot], axis = 1))
+        return pd.concat([summary, pivot], axis = 1)
