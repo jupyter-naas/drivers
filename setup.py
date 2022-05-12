d9af673c35acbff513505a25e467710e905a375b
Maxime Jublou <maxime@jublou.fr>
2022-05-12 18:04:07 +0200

    fix: Version drift

--- setup.py
+++ setup.py
@@ -136,7 +136,7 @@ extras_require['all'] = extras_require['full']
 
 setup(
     name="naas-drivers",
-    version="0.90.0b1",
+    version="0.96.0",
     author="Maxime Jublou",
     author_email="maxime@naas.ai",
     license="BSD",
