209248328177297456139800e17cdfe35e4e89ae
Maxime Jublou <maxime@jublou.fr>
2022-05-13 10:55:03 +0200

    fix: Bump numpy version

--- setup.py
+++ setup.py
@@ -151,7 +151,7 @@ setup(
         "pandas-datareader==0.9.0",
         "requests==2.25.1",
         "mprop==0.16.0",
-        "numpy==1.22.3"
+        "numpy~=1.19.0"
     ],
     classifiers=[
         "Programming Language :: Python :: 3.8",
