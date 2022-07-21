469e61466aef58cd348d998e84d0e4550b23fe01
Maxime Jublou <maxime@jublou.fr>
2022-07-21 15:33:04 +0200

    fix(Streamlit): Remove ngrok and update streamlit version (#303)
    * fix(Streamlit): Remove ngrok and update streamlit version
    
    * fix(Streamlit): Improved logging

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ extras_require = {
     ],
     "streamlit": [
         "pyngrok==5.0.5",
-        "streamlit==1.9.0",
+        "streamlit>=1.8.1",
     ],
     "teams": [
         "pymsteams==0.1.14",
