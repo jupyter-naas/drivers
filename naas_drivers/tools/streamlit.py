2d610aeec4f2a503d1591a142bd9f5d4a43f0630
Maxime Jublou <maxime@jublou.fr>
2022-07-21 12:35:37 +0200

    fix(Streamlit): Remove ngrok and update streamlit version

--- naas_drivers/tools/streamlit.py
+++ naas_drivers/tools/streamlit.py
@@ -1,19 +1,7 @@
 import os
 import subprocess
-from pyngrok import ngrok
 
-
-class BaseApp:
-    def _start_server(self, port):
-        active_tunnels = ngrok.get_tunnels()
-        for tunnel in active_tunnels:
-            public_url = tunnel.public_url
-            ngrok.disconnect(public_url)
-        url = ngrok.connect(addr=port, options={"bind_tls": True})
-        print(f"Web App can be accessed on: {url}")
-
-
-class Streamlit(BaseApp):
+class Streamlit():
     """
     Naas Streamlit app
     """
@@ -21,7 +9,6 @@ class Streamlit(BaseApp):
     def add(self, path="app.py", port=9999, debug=True):
         self.path = path
         self.port = port
-        self._start_server(port)
         os.system(f"fuser -n tcp -k {self.port}")
         cmd = f"streamlit run {self.path} --server.port {self.port}"
         with subprocess.Popen(
